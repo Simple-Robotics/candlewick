6ccc63e86d6fe10b8d78e333c7e48ac2a2e6673b
ManifoldFR <wilson.jallet@polytechnique.org>
2025-04-10 14:00:58 +0200

    examples/Visualizer.cpp : set to 50fps by default, add cli args

--- examples/Visualizer.cpp
+++ examples/Visualizer.cpp
@@ -6,12 +6,21 @@
 #include <pinocchio/algorithm/geometry.hpp>
 #include <chrono>
 
-// #include <CLI/App.hpp>
+#include <CLI/App.hpp>
+#include <CLI/Formatter.hpp>
+#include <CLI/Config.hpp>
 
 using namespace candlewick::multibody;
 using std::chrono::steady_clock;
 
-int main() {
+int main(int argc, char **argv) {
+  CLI::App app{"Visualizer example"};
+  argv = app.ensure_utf8(argv);
+  double fps;
+  app.add_option<double, unsigned int>("--fps", fps, "Framerate")
+      ->default_val(50);
+
+  CLI11_PARSE(app, argc, argv);
 
   pin::Model model;
   pin::GeometryModel geom_model;
@@ -24,8 +33,8 @@ int main() {
   Eigen::VectorXd q0 = pin::neutral(model);
   Eigen::VectorXd q1 = pin::randomConfiguration(model);
 
-  constexpr double dt = 0.04;
-  constexpr std::chrono::duration<double> dt_ms{dt};
+  double dt = 1. / static_cast<double>(fps);
+  std::chrono::duration<double> dt_ms{dt};
   static_assert(std::same_as<decltype(dt_ms.count()), double>);
   double t = 0.;
 
