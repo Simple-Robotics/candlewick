2044c801a809e1090967f34d1fdfab55cbf4be66
ManifoldFR <wilson.jallet@polytechnique.org>
2025-04-11 17:23:34 +0200

    Dummy pull/push system

--- src/candlewick/runtime/main.cpp
+++ src/candlewick/runtime/main.cpp
@@ -1,3 +1,32 @@
+#include <zmq.hpp>
+#include <zmq_addon.hpp>
 #include "candlewick/multibody/Multibody.h"
 
-int main() { return 0; }
+#include <pinocchio/serialization/model.hpp>
+#include <pinocchio/serialization/geometry.hpp>
+
+namespace pin = pinocchio;
+
+int main() {
+  zmq::context_t ctx;
+  zmq::socket_t sock{ctx, zmq::socket_type::pull};
+  sock.bind("tcp://127.0.0.1:12000");
+  const std::string endpoint = sock.get(zmq::sockopt::last_endpoint);
+  printf("ZMQ endpoint: %s\n", endpoint.c_str());
+
+  pin::Model model;
+  pin::GeometryModel geom_model;
+
+  std::vector<zmq::message_t> recv_models;
+
+  printf("Receiving Model and GeometryModel...");
+
+  const auto ret = zmq::recv_multipart(sock, std::back_inserter(recv_models));
+  assert(*ret == 2);
+
+  model.loadFromString(recv_models[0].to_string());
+  std::cout << model << std::endl;
+  geom_model.loadFromString(recv_models[1].to_string());
+
+  return 0;
+}
