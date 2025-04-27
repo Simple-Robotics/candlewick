2408ffc78e8711814492a5a6b1ee4c7c783bcbbc
ManifoldFR <wilson.jallet@polytechnique.org>
2025-04-27 21:37:06 +0200

    switch to nng

--- src/candlewick/multibody/VisualizerClient.h
+++ src/candlewick/multibody/VisualizerClient.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "Multibody.h"
+#include <nng/nng.h>
 #include <pinocchio/visualizers/base-visualizer.hpp>
-#include <zmq.h>
 
 namespace candlewick {
 namespace multibody {
   class VisualizerClient final : public pin::visualizers::BaseVisualizer {
-  public:
+    nng_socket *m_sock;
+    nng_dialer *m_dialer;
   };
 } // namespace multibody
 } // namespace candlewick
