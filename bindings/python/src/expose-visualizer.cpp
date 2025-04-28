733533a34dd53f5e2bc0ce7706e5fc368687c501
Wilson Jallet <wilson.jallet@polytechnique.org>
2025-04-28 15:20:44 +0200

    [python] Fix `Visualizer` getters for Pinocchio 3.5.0 (#43)
    * examples/python : add print test for viz.model
    
    * expose-visualizer.cpp : add fix for visualizer getters on Pinocchio 3.5.0

--- bindings/python/src/expose-visualizer.cpp
+++ bindings/python/src/expose-visualizer.cpp
@@ -4,6 +4,7 @@
 #include "candlewick/multibody/Visualizer.h"
 #include <pinocchio/bindings/python/visualizers/visualizer-visitor.hpp>
 
+#include <pinocchio/config.hpp>
 #include <pinocchio/multibody/model.hpp>
 #include <pinocchio/multibody/geometry.hpp>
 
@@ -14,11 +15,29 @@ void exposeVisualizer() {
   bp::class_<Visualizer::Config>("VisualizerConfig", bp::init<>())
       .def_readwrite("width", &Visualizer::Config::width)
       .def_readwrite("height", &Visualizer::Config::height);
-  bp::class_<Visualizer, boost::noncopyable>("Visualizer", bp::no_init)
-      .def(bp::init<Visualizer::Config, const pin::Model &,
-                    const pin::GeometryModel &>(
-          ("self"_a, "config", "model", "geomModel")))
-      .def(pinocchio::python::VisualizerPythonVisitor<Visualizer>{})
-      .def_readonly("renderer", &Visualizer::renderer)
-      .add_property("shouldExit", &Visualizer::shouldExit);
+
+  auto cl =
+      bp::class_<Visualizer, boost::noncopyable>("Visualizer", bp::no_init)
+          .def(bp::init<Visualizer::Config, const pin::Model &,
+                        const pin::GeometryModel &>(
+              ("self"_a, "config", "model", "geomModel")))
+          .def(pinocchio::python::VisualizerPythonVisitor<Visualizer>{})
+          .def_readonly("renderer", &Visualizer::renderer)
+          .add_property("shouldExit", &Visualizer::shouldExit);
+
+// fix for Pinocchio 3.5.0
+#if PINOCCHIO_VERSION_AT_MOST(3, 5, 0)
+#define DEF_PROP_PROXY(name)                                                   \
+  add_property(#name, bp::make_function(                                       \
+                          +[](Visualizer &v) -> auto & { return v.name(); },   \
+                          bp::return_internal_reference<>()))
+  cl //
+      .DEF_PROP_PROXY(model)
+      .DEF_PROP_PROXY(visualModel)
+      .DEF_PROP_PROXY(collisionModel)
+      .DEF_PROP_PROXY(data)
+      .DEF_PROP_PROXY(visualData)
+      .DEF_PROP_PROXY(collisionData);
+#undef DEF_PROP_PROXY
+#endif
 }
