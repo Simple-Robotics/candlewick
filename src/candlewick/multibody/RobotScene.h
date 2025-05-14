d489813bc34b112785258004d8d3173fab8ff319
ManifoldFR <wilson.jallet@polytechnique.org>
2025-05-14 14:21:06 +0200

    core/Scene.h : switch include of Renderer.h to Core.h

--- src/candlewick/multibody/RobotScene.h
+++ src/candlewick/multibody/RobotScene.h
@@ -6,6 +6,7 @@
 #include "Multibody.h"
 #include "../core/Device.h"
 #include "../core/Scene.h"
+#include "../core/Renderer.h"
 #include "../core/LightUniforms.h"
 #include "../core/Collision.h"
 #include "../core/DepthAndShadowPass.h"
