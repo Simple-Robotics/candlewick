ce66e5eb28e36495d786e7df50685c32965a3ea4
ManifoldFR <wilson.jallet@polytechnique.org>
2025-06-12 18:06:25 +0200

    LoadPinocchioGeometry.h : update docstring

--- src/candlewick/multibody/LoadPinocchioGeometry.h
+++ src/candlewick/multibody/LoadPinocchioGeometry.h
@@ -9,9 +9,19 @@ namespace candlewick::multibody {
 
 /// \brief Load an invidual Pinocchio GeometryObject's component geometries into
 /// an array of \c MeshData.
+///
+/// \note
+/// The mesh materials' base colors will be overriden by the
+/// gobj.meshColor attribute if either of the following
+/// are true:
+///   - the geometry's object type is coal::OT_GEOM,
+///   - the gobj.overrideMaterial flag is set to \c true.
+///
 void loadGeometryObject(const pin::GeometryObject &gobj,
                         std::vector<MeshData> &meshData);
 
+/// \copydoc loadGeometryObject(const pin::GeometryObject&,
+/// std::vector<MeshData> &)
 inline std::vector<MeshData>
 loadGeometryObject(const pin::GeometryObject &gobj) {
   std::vector<MeshData> meshData;
