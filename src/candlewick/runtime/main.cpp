6cccdbc34ac924f305ee4f70e7242b23e353cf4a
ManifoldFR <wilson.jallet@polytechnique.org>
2025-07-10 00:37:02 +0200

    poc sending models + matrices over zmq + msgpack

--- src/candlewick/runtime/main.cpp
+++ src/candlewick/runtime/main.cpp
@@ -16,19 +16,73 @@ namespace cdw = candlewick;
 namespace pin = pinocchio;
 using cdw::multibody::Visualizer;
 
-void receive_models(zmq::socket_ref sock, pin::Model &model,
-                    pin::GeometryModel &geom_model) {
-  std::vector<zmq::message_t> recv_models;
+constexpr std::string_view CMD_SEND_MODELS = "send_models";
+constexpr std::string_view CMD_SEND_STATE = "send_state";
 
-  printf("Receiving Model and GeometryModel...\n");
+msgpack::object_handle get_handle_from_zmq_msg(zmq::message_t &&msg) {
+  if (!msg.empty())
+    return msgpack::unpack(static_cast<const char *>(msg.data()), msg.size());
+  else
+    return msgpack::object_handle();
+}
 
-  const auto ret =
-      zmq::recv_multipart_n(sock, std::back_inserter(recv_models), 2);
-  assert(ret.has_value());
-  printf("Received %zu messages\n", ret.value());
+struct ArraySpec {
+  std::string dtype;
+  std::vector<long> dims;
+  std::vector<uint8_t> data;
 
-  model.loadFromString(recv_models[0].to_string());
-  geom_model.loadFromString(recv_models[1].to_string());
+  size_t ndim() const noexcept { return dims.size(); }
+
+  MSGPACK_DEFINE(dtype, dims, data);
+};
+
+template <typename MatrixType> auto get_eigen_view_from_spec(ArraySpec &spec) {
+  using Scalar = typename MatrixType::Scalar;
+  using MapType = Eigen::Map<MatrixType>;
+  Scalar *data = reinterpret_cast<Scalar *>(spec.data.data());
+  const Eigen::Index rows = spec.dims[0];
+  if constexpr (MatrixType::IsVectorAtCompileTime) {
+    return MapType{data, rows};
+  } else {
+    const Eigen::Index cols = spec.dims[1];
+    return MapType{data, rows, cols};
+  }
+}
+
+struct ApplicationContext {
+  pin::Model model;
+  pin::GeometryModel geom_model;
+};
+
+/// Handle first incoming message.
+bool handle_first_message(zmq::socket_ref sock, ApplicationContext &app_ctx) {
+  while (true) {
+    std::vector<zmq::message_t> msgs;
+
+    auto ret = zmq::recv_multipart_n(sock, std::back_inserter(msgs), 2);
+    if (!ret)
+      return false;
+
+    sock.send(zmq::str_buffer("ok"));
+    auto msg_header = msgs[0].to_string_view();
+    if (msg_header == CMD_SEND_MODELS) {
+      msgpack::object_handle oh = get_handle_from_zmq_msg(std::move(msgs[1]));
+      auto obj = oh.get();
+      std::array<std::string, 2> model_strings;
+      obj.convert(model_strings);
+      app_ctx.model.loadFromString(model_strings[0]);
+      app_ctx.geom_model.loadFromString(model_strings[1]);
+      return true;
+    } else {
+      SDL_Log("First message must have header \'%s\', got \'%s\'. Retry.",
+              CMD_SEND_MODELS.data(), msg_header.data());
+      continue;
+    }
+  }
+
+  SDL_Log("Loaded model with %d joints", app_ctx.model.njoints);
+  SDL_Log("Loaded geometry model with %zd gobjs", app_ctx.geom_model.ngeoms);
+  return false;
 }
 
 int main(int argc, char **argv) {
@@ -41,26 +95,41 @@ int main(int argc, char **argv) {
 
   CLI11_PARSE(app, argc, argv);
 
-  if (window_dims.size() != 2) {
-    cdw::terminate_with_message("Expected only two values for argument --dims");
-  }
-
   zmq::context_t ctx;
-  zmq::socket_t sock{ctx, zmq::socket_type::pull};
+  zmq::socket_t sock{ctx, zmq::socket_type::rep};
   sock.bind("tcp://127.0.0.1:12000");
   const std::string endpoint = sock.get(zmq::sockopt::last_endpoint);
-  printf("ZMQ endpoint: %s\n", endpoint.c_str());
+  SDL_Log("ZMQ endpoint: %s", endpoint.c_str());
 
-  pin::Model model;
-  pin::GeometryModel geom_model;
-  receive_models(sock, model, geom_model);
+  // ===== Runtime application context =====
+  ApplicationContext app_ctx;
+
+  // Handle first message
+  bool loaded_models = handle_first_message(sock, app_ctx);
+  if (!loaded_models)
+    return 1;
+
+  std::vector<zmq::message_t> msgs;
+  auto ret = zmq::recv_multipart_n(sock, std::back_inserter(msgs), 2);
+  assert(ret);
+  sock.send(zmq::str_buffer("ok2"));
+  Eigen::VectorXd q0;
+  if (msgs[0].to_string_view() == CMD_SEND_STATE) {
+    auto oh = get_handle_from_zmq_msg(std::move(msgs[1]));
+    auto obj = oh.get();
+    ArraySpec array_spec = obj.as<ArraySpec>();
+    Eigen::Map mapped_matrix =
+        get_eigen_view_from_spec<Eigen::VectorXd>(array_spec);
+    std::cout << "Mapped matrix:\n" << mapped_matrix.transpose() << std::endl;
+    q0 = mapped_matrix;
+  } else {
+    cdw::terminate_with_message("Wrong message input. Quitting.");
+  }
 
   Visualizer::Config config;
   config.width = window_dims[0];
   config.height = window_dims[1];
-  Visualizer viz{config, model, geom_model};
-
-  Eigen::VectorXd q0 = pin::neutral(model);
+  Visualizer viz{config, app_ctx.model, app_ctx.geom_model};
 
   while (!viz.shouldExit()) {
     viz.display(q0);
--- tests/test_async_client.py
+++ tests/test_async_client.py
@@ -1,19 +1,76 @@
 import pinocchio as pin
 import example_robot_data as erd
 import zmq
+import numpy as np
+import msgspec
+
+from typing import Any, Type
 
 
+NUMPY_TYPE_CODE = 1
 PORT = 12000
 
-ctx = zmq.Context.instance()
-sock: zmq.Socket = ctx.socket(zmq.SocketType.PUSH)
-sock.connect(f"tcp://127.0.0.1:{PORT}")
 
-robot: pin.RobotWrapper = erd.load("solo12")
-model = robot.model
-geom_model = robot.visual_model
+class NumpySpec(msgspec.Struct, gc=False, array_like=True):
+    dtype: str
+    shape: tuple[int, ...]
+    data: bytes
 
-model_str = model.saveToString()
-geom_str = geom_model.saveToString()
-sock.send_multipart([model_str.encode(), geom_str.encode()])
-print("Sent")
+    def to_numpy(self):
+        return np.frombuffer(self.data, dtype=self.dtype).reshape(self.shape)
+
+    @classmethod
+    def from_numpy(cls, arr: np.ndarray) -> "NumpySpec":
+        return cls(str(arr.dtype), arr.shape, arr.data)
+
+
+def numpy_enc_hook(obj: Any):
+    if isinstance(obj, np.ndarray):
+        return NumpySpec.from_numpy(obj)
+    return obj
+
+
+def numpy_dec_hook(type: Type, obj: Any):
+    print("dec_hook")
+    print(type, obj)
+    if type is NumpySpec:
+        return obj.to_numpy()
+    else:
+        raise NotImplementedError("Other objects supported")
+
+
+_encoder = msgspec.msgpack.Encoder(enc_hook=numpy_enc_hook)
+_decoder = msgspec.msgpack.Decoder(dec_hook=numpy_dec_hook)
+
+
+def send_models(sock: zmq.Socket, model_str, geom_str):
+    """Send Pinocchio model and geometry model as strings."""
+    sock.send_multipart([b"send_models", _encoder.encode([model_str, geom_str])])
+
+
+def send_state(sock: zmq.Socket, q: np.ndarray, v: np.ndarray | None = None):
+    payload = _encoder.encode(q)
+    sock.send_multipart([b"send_state", payload])
+
+
+if __name__ == "__main__":
+    ctx = zmq.Context.instance()
+    sock: zmq.Socket = ctx.socket(zmq.SocketType.REQ)
+    url = f"tcp://127.0.0.1:{PORT}"
+    sock.connect(addr=url)
+
+    robot: pin.RobotWrapper = erd.load("ur3")
+    model: pin.Model = robot.model
+    geom_model: pin.GeometryModel = robot.visual_model
+
+    model_str = model.saveToString()
+    geom_str = geom_model.saveToString()
+    send_models(sock, model_str, geom_str)
+    response = sock.recv().decode()
+    print(response)
+
+    # q0 = pin.neutral(model)
+    q0 = pin.randomConfiguration(model)
+    send_state(sock, q0)
+    response = sock.recv().decode()
+    print(response)
