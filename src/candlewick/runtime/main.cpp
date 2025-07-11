9c6b67c459bcb2f308ba8b786ef98e98c6ea4eb1
ManifoldFR <wilson.jallet@polytechnique.org>
2025-07-11 14:56:04 +0200

    runtime : add camera pose endpoint, and for `viz.clean()`
    + receive messages into fixed-size buffer of size 2

--- src/candlewick/runtime/main.cpp
+++ src/candlewick/runtime/main.cpp
@@ -19,6 +19,9 @@ using cdw::multibody::Visualizer;
 
 constexpr std::string_view CMD_SEND_MODELS = "send_models";
 constexpr std::string_view CMD_SEND_STATE = "state_update";
+constexpr std::string_view CMD_SEND_CAM_POSE = "send_cam_pose";
+constexpr std::string_view CMD_CLEAN = "cmd_clean";
+using RowMat4d = Eigen::Matrix<pin::context::Scalar, 4, 4, Eigen::RowMajor>;
 
 msgpack::object_handle get_handle_from_zmq_msg(zmq::message_t &&msg) {
   if (!msg.empty())
@@ -37,34 +40,54 @@ struct ArrayMessage {
   MSGPACK_DEFINE(dtype, dims, data);
 };
 
+namespace detail {
+template <typename MapType, typename P>
+auto get_eigen_view_msg_impl(const ArrayMessage &spec, P *data) {
+  using PlainObject = typename MapType::PlainObject;
+  const Eigen::Index rows = spec.dims[0];
+  if constexpr (PlainObject::IsVectorAtCompileTime) {
+    return MapType{data, rows};
+  } else {
+    const Eigen::Index cols = spec.ndim() == 2
+                                  ? spec.dims[1]
+                                  : 1; // assume runtime vector if ndim wrong
+    return MapType{data, rows, cols};
+  }
+}
+} // namespace detail
+
+/// \brief Convert ArrayMessage to a mutable Eigen::Matrix view.
 template <typename MatrixType>
 auto get_eigen_view_from_spec(ArrayMessage &spec) {
   using Scalar = typename MatrixType::Scalar;
   using MapType = Eigen::Map<MatrixType>;
   Scalar *data = reinterpret_cast<Scalar *>(spec.data.data());
-  const Eigen::Index rows = spec.dims[0];
-  if constexpr (MatrixType::IsVectorAtCompileTime) {
-    return MapType{data, rows};
-  } else {
-    const Eigen::Index cols = spec.dims[1];
-    return MapType{data, rows, cols};
-  }
+  return detail::get_eigen_view_msg_impl<MapType>(spec, data);
+}
+
+/// \copydoc get_eigen_view_from_spec()
+template <typename MatrixType>
+auto get_eigen_view_from_spec(const ArrayMessage &spec) {
+  using Scalar = typename MatrixType::Scalar;
+  using MapType = Eigen::Map<const MatrixType>;
+  const Scalar *data = reinterpret_cast<const Scalar *>(spec.data.data());
+  return detail::get_eigen_view_msg_impl<MapType>(spec, data);
 }
 
 struct ApplicationContext {
   pin::Model model;
   pin::GeometryModel geom_model;
   zmq::context_t ctx{};
-  zmq::socket_t setup_sock{ctx, zmq::socket_type::rep};
+  zmq::socket_t sync_sock{ctx, zmq::socket_type::rep};
   zmq::socket_t state_sock{ctx, zmq::socket_type::sub};
 };
 
 /// Handle first incoming message.
 bool handle_first_message(zmq::socket_ref sock, ApplicationContext &app_ctx) {
   while (true) {
-    std::vector<zmq::message_t> msgs;
+    std::array<zmq::message_t, 2> msgs;
 
-    auto ret = zmq::recv_multipart_n(sock, std::back_inserter(msgs), 2);
+    auto ret = zmq::recv_multipart_n(sock, msgs.begin(), 2);
     if (!ret)
       return false;
 
@@ -95,17 +118,16 @@ bool handle_first_message(zmq::socket_ref sock, ApplicationContext &app_ctx) {
 void run_main_loop(Visualizer &viz, ApplicationContext &app_ctx) {
 
   while (!viz.shouldExit()) {
-    std::vector<zmq::message_t> msgs;
-    auto rec =
-        zmq::recv_multipart_n(app_ctx.state_sock, std::back_inserter(msgs), 2,
-                              zmq::recv_flags::dontwait);
+    std::array<zmq::message_t, 2> msgs;
+    auto rec = zmq::recv_multipart_n(app_ctx.state_sock, msgs.begin(), 2,
+                                     zmq::recv_flags::dontwait);
 
     if (rec) {
       // route through message headers
       auto header = msgs[0].to_string_view();
       if (header == CMD_SEND_STATE) {
         auto oh = get_handle_from_zmq_msg(std::move(msgs[1]));
-        auto obj = oh.get();
+        msgpack::object obj = oh.get();
         std::tuple<ArrayMessage, std::optional<ArrayMessage>> arrays;
         obj.convert(arrays);
         auto [q_msg, v_msg] = arrays;
@@ -116,6 +138,23 @@ void run_main_loop(Visualizer &viz, ApplicationContext &app_ctx) {
           auto v_view = get_eigen_view_from_spec<Eigen::VectorXd>(*v_msg);
           pin::forwardKinematics(viz.model(), viz.data(), q_view, v_view);
         }
+      }
+    }
+
+    rec = zmq::recv_multipart_n(app_ctx.sync_sock, msgs.begin(), 2,
+                                zmq::recv_flags::dontwait);
+    if (rec) {
+      auto header = msgs[0].to_string_view();
+      if (header == CMD_SEND_CAM_POSE) {
+        auto oh = get_handle_from_zmq_msg(std::move(msgs[1]));
+        msgpack::object obj = oh.get();
+        ArrayMessage M_msg = obj.as<ArrayMessage>();
+        auto M = get_eigen_view_from_spec<RowMat4d>(M_msg);
+        viz.setCameraPose(M);
+        app_ctx.sync_sock.send(zmq::str_buffer("ok"));
+      } else if (header == CMD_CLEAN) {
+        viz.clean();
+        app_ctx.sync_sock.send(zmq::str_buffer("ok"));
       }
     }
 
@@ -140,24 +179,22 @@ int main(int argc, char **argv) {
 
   // ===== Runtime application context =====
   ApplicationContext app_ctx;
-  zmq::socket_t &setup_sock = app_ctx.setup_sock;
+  zmq::socket_t &sync_sock = app_ctx.sync_sock;
   zmq::socket_t &state_sock = app_ctx.state_sock;
-  setup_sock.bind(std::format("tcp://{:s}:{:d}", hostname, port));
+  sync_sock.bind(std::format("tcp://{:s}:{:d}", hostname, port));
   state_sock.bind(std::format("tcp://{:s}:{:d}", hostname, port + 2));
   state_sock.set(zmq::sockopt::subscribe, CMD_SEND_STATE);
 
   std::string endpoint;
-  endpoint = setup_sock.get(zmq::sockopt::last_endpoint);
+  endpoint = sync_sock.get(zmq::sockopt::last_endpoint);
   SDL_Log("ZMQ endpoint (setup): %s", endpoint.c_str());
   endpoint = state_sock.get(zmq::sockopt::last_endpoint);
   SDL_Log("ZMQ endpoint (state): %s", endpoint.c_str());
 
   // Handle first message
-  bool loaded_models = handle_first_message(setup_sock, app_ctx);
+  bool loaded_models = handle_first_message(sync_sock, app_ctx);
   if (!loaded_models)
     return 1;
-
-  setup_sock.close();
 
   Visualizer::Config config;
   config.width = window_dims[0];
